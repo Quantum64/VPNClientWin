895a1942ad6466cc90c672158470d996b4ab229d
Dylan <Quantum64Development@gmail.com>
2017-01-04 09:40:32 -0500

    Rename form

---
+++ VPNClient/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+
+namespace VPNClient {
+    public partial class MainForm : Form {
+        public ChromiumWebBrowser chrome;
+
+        public MainForm() {
+            this.InitializeComponent();
+        }
+
+        private void initBrowser() {
+
+        }
+    }
+}
+    
