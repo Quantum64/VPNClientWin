d3991b512772ca242b5708b5a1cf6a8d17c385bd
Dylan <Quantum64Development@gmail.com>
2017-01-04 13:54:52 -0500

    Add CEFSharp

--- VPNClient/MainForm.cs
+++ VPNClient/MainForm.cs
@@ -7,19 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CefSharp;
 using CefSharp.WinForms;
 
 namespace VPNClient {
     public partial class MainForm : Form {
-        public ChromiumWebBrowser chrome;
+        private ChromiumWebBrowser chrome { get; }
+        private double factor = 1.0f;
 
         public MainForm() {
+            this.AutoScaleMode = AutoScaleMode.Font;
             this.InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.chrome = new ChromiumWebBrowser("https://q64.co") {
+                Dock = DockStyle.Fill,
+            };
+            chrome.FrameLoadEnd += browserOnFrameLoadEnd;
+            int currentDPI = 0;
+            using (Graphics g = this.CreateGraphics()) {
+                currentDPI = (int) g.DpiX;
+            }
+            this.factor = currentDPI / 96.0;
+            this.Controls.Add(chrome);
         }
 
         private void initBrowser() {
 
         }
+
+        private void browserOnFrameLoadEnd(object sender, FrameLoadEndEventArgs frameLoadEndEventArgs) {
+            ChromiumWebBrowser browser = (ChromiumWebBrowser)sender;
+            //Dispatcher.Invoke(() =>
+            //{
+             //   ZoomLevelTextBox.Text = ((Convert.ToDouble(browser.Tag) - 100) / 25.0).ToString(CultureInfo.CurrentCulture);
+                browser.SetZoomLevel(factor * 2);
+            //});
+        }
     }
 }
-    
--- VPNClient/Program.cs
+++ VPNClient/Program.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CefSharp;
 
 namespace VPNClient {
     static class Program {
         [STAThread]
         static void Main() {
+            if (Environment.OSVersion.Version.Major >= 6) {
+                SetProcessDPIAware();
+            }
+            var settings = new CefSettings();
+            settings.CefCommandLineArgs.Add("disable-plugins-discovery", "1");
+            settings.CefCommandLineArgs.Add("no-proxy-server", "1");
+            settings.CefCommandLineArgs.Add("disable-gpu-vsync", "1");
+            settings.CefCommandLineArgs.Add("disable-gpu", "1");
+            settings.CefCommandLineArgs.Add("disable-direct-write", "1");
+            settings.CefCommandLineArgs.Add("high-dpi-support", "1");
+            settings.CefCommandLineArgs.Add("force-device-scale-factor", "1");
+            settings.CefCommandLineArgs.Add("multi-threaded-message-loop", "1");
+            settings.CefCommandLineArgs.Add("off-screen-rendering-enabled", "1");
+            Cef.Initialize(settings);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
